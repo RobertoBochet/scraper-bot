73d1dae7c961cc66a0625bafdb7b16a87aec696e
Roberto Bochet <robertobochet@gmail.com>
2021-11-25 22:29:01 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.10.0-alpine as compiler
+
+WORKDIR /srv
+
+COPY setup.py ./
+COPY README.md ./
+COPY bot_scraper ./bot_scaper
+
+RUN python3 setup.py sdist bdist_wheel
+
+FROM python:3.10.0-alpine
+
+VOLUME /srv
+
+COPY --from=compiler /srv/dist/*.whl /
+
+RUN pip3 install *.whl
+
+ENTRYPOINT python3 -m bot_scraper
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+
+__version__ = "0.0"
+exec(open("./bot_scraper/version.py").read())
+
+with open("README.md") as f:
+    _LONG_DESCRIPTION = f.read()
+
+setup(
+    name="bot-scraper",
+    packages=find_packages(),
+    version=__version__,
+    license="gpl-3.0",
+    description="A telegram bot to stay tunded on real estate ads",
+    long_description=_LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    author="Roberto Bochet",
+    author_email="robertobochet@gmail.com",
+    url="https://github.com/RobertoBochet/bot-scraper",
+    license_files=("LICENSE",),
+    keywords=["bot", "telegrambot", "scraper", "telegram"],
+    install_requires=[
+        "python-telegram-bot ~= 13.8.1",
+        "beautifulsoup4 ~= 4.10.0",
+        "redis ~= 4.0.2",
+        "requests ~= 2.26.0",
+    ],
+    extras_require={"dev": ["pre-commit ~= 2.15.0"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.10",
+)
