47c6395016e7a81264cb26e046d94ff0ee32438a
Roberto Bochet <robertobochet@gmail.com>
2021-11-27 16:53:11 +0100

    Make Task a subclass of Scraper

--- bot_scraper/bot_scraper/_task.py
+++ bot_scraper/bot_scraper/_task.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Callable
 
 from ischedule import schedule
 
@@ -7,30 +8,28 @@ from ..scraper import Scraper
 _LOGGER = logging.getLogger(__package__)
 
 
-class Task:
+class Task(Scraper):
     name: str
     interval: int
-    scraper: Scraper
 
     def __init__(
-        self, interval: int = 60 * 60, name: str = "generic-task", **kwargs
+        self,
+        url: str,
+        target: str,
+        on_find: Callable[[...], None],
+        interval: int = 60 * 60,
+        name: str = "generic-task",
     ):
+        super().__init__(url, target, on_find)
         self.name = name
         self.interval = interval
-        self.scraper = Scraper(**kwargs, on_find=self._on_find)
 
         _LOGGER.info(f"Created task {self.name}")
 
     def schedule(self):
-        schedule(self._work, interval=self.interval)
+        schedule(self.run, interval=self.interval)
 
         _LOGGER.info(f"Scheduled task {self.name}")
-
-    def _work(self):
-        self.scraper.run()
-
-    def _on_find(self, *args: list[str]):
-        print(args)
 
     @classmethod
     def make(cls, config: dict):
