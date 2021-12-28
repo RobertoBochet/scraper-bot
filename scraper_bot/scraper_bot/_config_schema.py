900bfa607ff41be5a90e0f4712b9203a89c0d620
Roberto Bochet <robertobochet@gmail.com>
2021-12-28 23:08:08 +0100

    Implemented schema in python file

---
+++ scraper_bot/scraper_bot/_config_schema.py
@@ -0,0 +1,33 @@
+CONFIG_SCHEMA = {
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$id": "scraper_bot/config.schema.json",
+    "title": "Scraper bot configuration",
+    "type": "object",
+    "properties": {
+        "bot": {
+            "type": "object",
+            "properties": {
+                "token": {"type": "string"},
+                "chats": {
+                    "type": "array",
+                    "items": [{"type": "integer"}, {"type": "integer"}],
+                },
+            },
+            "required": ["token", "chats"],
+        },
+        "tasks": {"type": "array", "items": {"$ref": "#/$defs/Task"}},
+    },
+    "$defs": {
+        "Task": {
+            "type": "object",
+            "properties": {
+                "name": {"type": "string"},
+                "url": {"type": "string"},
+                "target": {"type": "string"},
+                "interval": {"type": "integer"},
+            },
+            "required": ["name", "url", "target", "interval"],
+        }
+    },
+    "required": ["bot", "tasks"],
+}
